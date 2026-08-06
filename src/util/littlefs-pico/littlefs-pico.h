1244ffa305a22874788cb0b986e6ad8c19564ec9
Vesek <venda.straka@gmail.com>
2026-08-06 18:11:32 +0200

    Update linker scripts for `pico-sdk` 2.3.0
    Includes update of Eternity

--- src/util/littlefs-pico/littlefs-pico.h
+++ src/util/littlefs-pico/littlefs-pico.h
@@ -9,8 +9,8 @@
 // This is setup to read a block of the flash from the end
 // ADDR_LITTLEFS is defined by the linker script
 // LITTLEFS_SIZE_BYTES is defined in CMakeLists.txt
-extern uint32_t ADDR_LITTLEFS[];
-#define LITTLEFS_BASE_ADDR (uint32_t)(ADDR_LITTLEFS)
+extern uint32_t __littlefs_start[];
+#define LITTLEFS_BASE_ADDR (uint32_t)(__littlefs_start)
 #define LITTLEFS_FLASH_OFFSET_ADDR (LITTLEFS_BASE_ADDR-XIP_BASE)
 
 int pico_lfs_init(lfs_t *lfs);
